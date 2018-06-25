a784f214339ed50ab90365df1f9754826156b90e
Shankar Choudhury <shan.choudhury@gmail.com>
2018-06-24 22:11:15 -0500

    initial mvn commit
    Signed-off-by: Shankar Choudhury <shan.choudhury@gmail.com>

---
+++ src/test/java/demoPack/CiTest.java
@@ -0,0 +1,13 @@
+package demoPack;
+
+public class CiTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a demo test to validate Travis Integration");
+	}
+
+}
