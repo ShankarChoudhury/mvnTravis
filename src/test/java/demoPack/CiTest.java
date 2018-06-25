d31490a2cf12e870ad3bf1c97ed6c788bf58c014
Shankar Choudhury <shan.choudhury@gmail.com>
2018-06-24 22:34:28 -0500

    ok
    Signed-off-by: Shankar Choudhury <shan.choudhury@gmail.com>

--- src/test/java/demoPack/CiTest.java
+++ src/test/java/demoPack/CiTest.java
@@ -1,13 +1,33 @@
 package demoPack;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class CiTest {
 
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		System.out.println("This is a demo test to validate Travis Integration");
+		
+		System.out.println("This is a demo test to validate Travis Integration");
+		
+		String loc ="C:\\java-workspace\\applications\\demo.txt";
+		
+		FileWriter file = new FileWriter(loc,true);
+		
+		BufferedWriter print = new BufferedWriter(file);
+	//	print.newLine();
+		print.write("Hi! this is a test mesage"+"\n");
+		print.newLine();
+	//	print.append("1");
+		
+		print.close();
+		file.close();
 	}
 
 }
